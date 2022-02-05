52ff0ec9bb082d8ab60fe816917d1adeb273ba1f
jose031199 <luisking531@gmail.com>
2022-02-05 11:26:13 -0800

    Class to register user

---
+++ js/person.js
@@ -0,0 +1,15 @@
+export class Person {
+    constructor(name){
+        this.name = name;
+        //this.lastname = lastname;
+        //this.phone = phone;
+        //this.email = email;
+        //this.dateRegister = dateRegister;
+        //this.typeOfRegister = typeOfRegister;
+    }
+
+    Show_Info(){
+        return "Registro Exitoso"
+    }
+}
+
